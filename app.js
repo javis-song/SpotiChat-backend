69d7cd913746978d93d0b237081488f6b99aacbe
Jiahua Song <song@Jiahuas-MacBook-Pro.local>
2020-10-16 23:17:00 -0400

    add socket

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ const io = require('socket.io')(http);
 const path = require('path');
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + "/index.html");
 });
 
 io.on('connection', (socket) => {
+    io.emit('chat message', "hello");
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
+        console.log(msg);
     });
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
     console.log('Press Ctrl+C to quit.');
 });
