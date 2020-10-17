e437b24d2b4273dcd950399b8ebfa4184101b7d0
Jiahua Song <song@Jiahuas-MacBook-Pro.local>
2020-10-17 14:29:23 -0400

    socket

--- app.js
+++ app.js
@@ -3,16 +3,29 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
+let queue = [];
+let connections = 0;
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
 io.on('connection', (socket) => {
-    io.emit('chat message', "hello");
+    socket.on('username', (msg) => {
+        connections++;
+        socket.username = msg;
+        queue.push(msg);
+        console.log(msg + "connects");
+    })
+
+    socket.emit('chat message', "xxx is in connectino with you!");
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
         console.log(msg);
     });
+
+    socket.on('disconnect', () => {
+        console.log(socket.username + "disconnect");
+    })
 });
 
 const PORT = process.env.PORT || 8080;
